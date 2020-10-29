94f81f8ac89b3314a2715577b927b16d70722e8f
e <454867353@qq.com>
2020-10-29 14:44:34 +0800

    路由懒加载

--- myapp/src/Router/index.js
+++ myapp/src/Router/index.js
@@ -1,19 +1,26 @@
-import React,{useLayoutEffect,useState,useContext} from 'react';
+import React,{useLayoutEffect,useState,useContext,Suspense,lazy} from 'react';
 import {Route,Switch,withRouter,Redirect} from 'react-router-dom'
 import {Col,Row} from 'antd'
 
 import Header from '../views/Header'
-
+// const MainContainer = lazy(() => import("../views/MainContainer"))
 import MainContainer from '../views/MainContainer'
+// const NavTab = lazy(() => import("../views/NavTab"))
 import NavTab from '../views/NavTab'
-import User from '../views/MainContainer/user'
-import Goods from '../views/MainContainer/goods'
-import Publish from '../views/MainContainer/publish'
-import Comments from '../views/MainContainer/comments'
-
-import Login from '../views/Login';
-import Reg from '../views/Reg';
-import NotFound from '../views/NotFound/404'
+const User = lazy(() => import("../views/MainContainer/user"))
+// import User from '../views/MainContainer/user'
+const Goods = lazy(() => import("../views/MainContainer/goods"))
+// import Goods from '../views/MainContainer/goods'
+const Publish = lazy(() => import("../views/MainContainer/publish"))
+// import Publish from '../views/MainContainer/publish'
+const Comments = lazy(() => import("../views/MainContainer/comments"))
+// import Comments from '../views/MainContainer/comments'
+const Login = lazy(() => import("../views/Login"))
+// import Login from '../views/Login';
+const Reg = lazy(() => import("../views/Reg"))
+// import Reg from '../views/Reg';
+const NotFound = lazy(() => import("../views/NotFound/404"))
+// import NotFound from '../views/NotFound/404'
 
 import {MyContext} from '../hook/index'
 
@@ -43,6 +50,7 @@ function RouterTable (props){
            
                                 <NavTab />
                                 <MainContainer>
+                                <Suspense fallback={<div>loading...</div>}>
                                         <Switch>
                                             <Route path="/mainContainer/user" component={User}/>
                                             <Route path="/mainContainer/goods" component={Goods}/>
@@ -52,10 +60,11 @@ function RouterTable (props){
                                             <Redirect from="/mainContainer" to="mainContainer/user" exact/>
                                             <Redirect path="/mainContainer" to="/notfound" />
                                         </Switch>
-                                     
+                                        </Suspense>    
                                 </MainContainer>
                    
                         </Row>
+                        <Suspense fallback={<div>loading...</div>}>
                     <Switch>
                         <Route path="/login" component={Login} />
                         <Route path="/Reg" component={Reg} />
@@ -64,15 +73,18 @@ function RouterTable (props){
                         <Redirect from="/" to="mainContainer/user" exact/>
                         <Redirect to="/notfound"/>
                     </Switch> 
+                    </Suspense>    
                 </>
                 :
                 <>
+                  <Suspense fallback={<div>loading...</div>}>
                 <Switch>
                         <Route path="/login" component={Login} />
                         <Route path="/Reg" component={Reg} />
                         <Route path="/notfound" component={NotFound}/>
                         <Redirect from="/" to="/login" />
                 </Switch>
+                </Suspense>
                 </>
             }
         </>
