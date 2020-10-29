07af144c8c7ba35d805ca4d33c6997c2f9e98fb8
chenzhenan <18718821135@163.com>
2020-10-29 22:11:15 +0800

    发布列表、删除、分页、排序、新增（50%）

--- myapp/src/Router/index.js
+++ myapp/src/Router/index.js
@@ -1,20 +1,28 @@
-import React,{useLayoutEffect,useState,useContext} from 'react';
+import React,{useLayoutEffect,useState,useContext,lazy,Suspense} from 'react';
 import {Route,Switch,withRouter,Redirect} from 'react-router-dom'
 import {Col,Row} from 'antd'
 
-import Header from '../views/Header'
+// import Header from '../views/Header'
+const Header = lazy(()=>import('../views/Header'))
+const MainContainer  = lazy(()=>import('../views/MainContainer'))
+const NavTab = lazy(()=>import('../views/NavTab'))
+const User = lazy(()=>import('../views/MainContainer/user'))
+const Goods = lazy(()=>import('../views/MainContainer/goods'))
+const Publish = lazy(()=>import('../views/MainContainer/publish'))
+const Comments = lazy(()=>import('../views/MainContainer/comments'))
+const Login = lazy(()=>import('../views/Login'))
+const Reg = lazy(()=>import('../views/Reg'))
+const NotFound = lazy(()=>import('../views/NotFound/404'))
+// import MainContainer from '../views/MainContainer'
+// import NavTab from '../views/NavTab'
+// import User from '../views/MainContainer/user'
+// import Goods from '../views/MainContainer/goods'
+// import Publish from '../views/MainContainer/publish'
+// import Comments from '../views/MainContainer/comments'
 
-import MainContainer from '../views/MainContainer'
-import NavTab from '../views/NavTab'
-import User from '../views/MainContainer/user'
-import Goods from '../views/MainContainer/goods'
-import Publish from '../views/MainContainer/publish'
-import Comments from '../views/MainContainer/comments'
-
-import Login from '../views/Login';
-import Reg from '../views/Reg';
-import NotFound from '../views/NotFound/404'
-
+// import Login from '../views/Login';
+// import Reg from '../views/Reg';
+// import NotFound from '../views/NotFound/404'
 import {MyContext} from '../hook/index'
 
 import './index.scss'
@@ -43,7 +51,7 @@ function RouterTable (props){
            
                                 <NavTab />
                                 <MainContainer>
-                                        <Switch>
+                                    <Switch>
                                             <Route path="/mainContainer/user" component={User}/>
                                             <Route path="/mainContainer/goods" component={Goods}/>
                                             <Route path="/mainContainer/publish" component={Publish}/>
@@ -51,28 +59,25 @@ function RouterTable (props){
                                             <Route path="/notfound" component={NotFound}/>
                                             <Redirect from="/mainContainer" to="mainContainer/user" exact/>
                                             <Redirect path="/mainContainer" to="/notfound" />
-                                        </Switch>
-                                     
+                                    </Switch>
                                 </MainContainer>
                    
                         </Row>
-                    <Switch>
-                        <Route path="/login" component={Login} />
-                        <Route path="/Reg" component={Reg} />
-                        <Route path="/mainContainer" component={MainContainer} />
-                        <Route path="/notfound" component={NotFound}/>
-                        <Redirect from="/" to="mainContainer/user" exact/>
-                        <Redirect to="/notfound"/>
-                    </Switch> 
+                        <Switch>
+                            <Route path="/login" component={Login} />
+                            <Route path="/Reg" component={Reg} />
+                            <Route path="/mainContainer" component={MainContainer} />
+                            <Route path="/notfound" component={NotFound}/>
+                            <Redirect from="/" to="mainContainer/user" exact/>
+                            <Redirect to="/notfound"/>
+                        </Switch> 
                 </>
                 :
                 <>
-                <Switch>
-                        <Route path="/login" component={Login} />
-                        <Route path="/Reg" component={Reg} />
-                        <Route path="/notfound" component={NotFound}/>
-                        <Redirect from="/" to="/login" />
-                </Switch>
+                    <Route path="/login" component={Login} />
+                    <Route path="/Reg" component={Reg} />
+                    <Route path="/notfound" component={NotFound}/>
+                    <Redirect from="/" to="/login" />
                 </>
             }
         </>
