9feb4b15284975a0e4393dd710fc1a6d2bde4b0c
e <454867353@qq.com>
2020-10-30 11:09:27 +0800

    测试

--- myapp/src/views/MainContainer/user/index.js
+++ myapp/src/views/MainContainer/user/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Table, Space,  Button,Modal,Form, Input} from 'antd';
+import { Table, Space,  Button,Modal,Form, Input,Popconfirm, message} from 'antd';
 import request from '@/utils/request'
 import './index.scss'
 const { Column } = Table;
@@ -7,9 +7,22 @@ class User extends React.Component {
   state = {
     data: [],
       visible: false,
-      isAdd:false
+      isAdd:false,
+      id:''
+      
     
   }
+
+ confirm=(e)=> {
+    console.log(e);
+    message.success('Click on Yes');
+  }
+  
+   cancel=(e)=> {
+    console.log(e);
+    message.error('Click on No');
+  }
+
   async componentDidMount() {
     const { data } = await request.get('user/list', {
       params: {
@@ -24,16 +37,22 @@ class User extends React.Component {
   }
 
 
-// 弹出框
+// 编辑弹出框
 
-// 确定编辑
-// handleOk = () => {
-//   this.setState({visible: false});
-//   // onFinish = values => {
-//   //   console.log('Success:', values);
-//   // };
-
-// };
+handleOkEdit = (id) => {
+  this.setState({visible: true,
+     isAdd:true,
+id:id
+    });
+ console.log(id);
+ 
+};
+// 新增弹出框
+handleOkAdd = () => {
+  this.setState({visible: true, isAdd:false});
+ 
+ 
+};
 // 取消编辑
 handleCancel = () => {
   this.setState({ visible: false });
@@ -41,20 +60,20 @@ handleCancel = () => {
 };
 // 新增用户
 Add=()=>{
-
   this.setState({
-    visible: true,
+    visible: false,
     isAdd:false
   });
 }
 
   // 编辑用户
   Edit=(id)=>{
-    console.log(id);
+    // console.log(id);
     this.setState({
-      visible: true,
+      visible: false,
       isAdd:true
     });
+    console.log(id);
   }
   onFinish = values => {
     console.log('Success:', values);
@@ -62,7 +81,10 @@ Add=()=>{
 
   // 删除用户
   Delete=(id)=>{
-    console.log(id);
+    
+      // const { data } = await request.get('user/delete/'+id)
+      //   console.log('data',data);
+        
   }
 
   
@@ -70,7 +92,7 @@ Add=()=>{
 
   render() {
     const { data,visible,isAdd} = this.state
-    console.log(data);
+    // console.log(data);
   
     return (
       <div className='user'>
@@ -85,24 +107,24 @@ Add=()=>{
         <Button type="primary" onClick={()=>{}}>
                   查询
                 </Button>  
-                <Button type="primary" onClick={this.Add}>
+                <Button type="primary" onClick={this.handleOkAdd}>
                 新增
                 </Button>
                 </Space></div>
       
 
 
-        <Table dataSource={data}>
-          <Column title="姓名" dataIndex="username" key="username" />
-          <Column title="注册时间" dataIndex="addTime" key="addTime" />
-          <Column title="头像" key="_id" render={(text, record, index) => (<img src={record.avatar} alt="" className='img' />)
+        <Table dataSource={data} rowKey="_id">
+          <Column title="姓名" dataIndex="username"  />
+          <Column title="注册时间" dataIndex="addTime"  />
+          <Column title="头像" render={(text, record, index) => (<img src={record.avatar} alt="" className='img' />)
           }/>
           <Column
             title="操作"
-            key="_id"
+    
             render={(text, record) => (
               <Space size="middle">
-                <Button type="primary" onClick={()=>{this.Edit(record._id)}}>
+                <Button type="primary" onClick={()=>{this.handleOkEdit(record._id)}}>
                   编辑
                 </Button>
                 <Button danger onClick={()=>{this.Delete(record._id)}}>
@@ -112,6 +134,17 @@ Add=()=>{
             )}
           />
         </Table>
+
+           {/* 删除弹框 */}
+           <Popconfirm
+    title="Are you sure delete this task?"
+    onConfirm={confirm}
+    onCancel={cancel}
+    okText="Yes"
+    cancelText="No"
+  >
+    <a href="#">Delete</a>
+  </Popconfirm>,
 
 
               {/* 编辑弹出框 */}
@@ -124,20 +157,19 @@ Add=()=>{
             <Button key="back" onClick={this.handleCancel}>
               取消
             </Button>,
-            <Button key="submit" type="primary" onClick={isAdd ? this.Add : this.Edit }
+            <Button key="submit" type="primary" onClick={isAdd ? this.Edit : this.Add }
             >
               确定
             </Button>,
           ]}
         >
         <Form layout="inline" style={{marginBottom: '10px'}} onFinish={this.onFinish}>
-        
-        <p><Form.Item label="用户">
+        <Form.Item label="用户">
             <Input type="text" placeholder="用户名" style={{width: '255px'}}/>
-          </Form.Item></p>  
-          <p><Form.Item label="密码">
+          </Form.Item> 
+          <Form.Item label="密码">
             <Input type="password" placeholder="密码" style={{width: '255px'}}/>
-          </Form.Item></p>
+          </Form.Item>
         </Form>
         </Modal>
       </div>
